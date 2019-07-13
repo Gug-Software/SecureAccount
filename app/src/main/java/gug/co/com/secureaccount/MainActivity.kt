9e6cf1c7e85da3a5468b661f3dd4984cd1f06bb9
JkVillavo12Col <jcmodev12@gmail.com>
2019-07-12 21:26:40 -0500

    Initial commit

---
+++ app/src/main/java/gug/co/com/secureaccount/MainActivity.kt
@@ -0,0 +1,12 @@
+package gug.co.com.secureaccount
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
